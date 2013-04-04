bf31221242b628d9658dd876b7afa63734a9a58f
Mohamed EL GHACHI <mohamed.elghachi@4d.com>
2013-04-04 16:52:06 +0000

    Initial commit

---
+++ NewDatastoreSolution/Untitled1.js
@@ -0,0 +1,22 @@
+﻿
+
+//var entityColAsc = ds.MyNullClass.orderBy('cnum asc');
+//entityColAsc;
+
+
+//ds.MyNullClass.all();
+
+
+//coll;
+//ds.MyClass2.all();//count('distinct')
+//var myEntity = ds.MyClass1.createEntity();
+
+
+//var myArray = ds.MyClass9.toArray("");
+//myArray;
+
+//ds.MyClass1.all();
+
+
+//var e = ds.MyClass1.createEntity();
+//var s  = ds.MyClass1.createEntity();
